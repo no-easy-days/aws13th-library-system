9eeedb2b9866f956d09f0758ae05083e262c0589
Eunice(Eunyoung) Shin <eunice.shin54@gmail.com>
2025-12-29 20:07:23 +0900

    feat: add start_program func - 첫 시작시 메뉴 제시 부분 완성

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import csv
 
 
 def read_file(filename):
+    # TODO: 같은 책 들어왔을 때 처리 필요
     books = []
     try:
         with open(filename, newline='', encoding='utf-8') as f:
@@ -10,10 +11,34 @@ def read_file(filename):
             next(reader)
             for title, author, isbn in reader:
                 books.append(Book(title, author, isbn))
+        print("\n[System] books.csv 에서 도서 데이터를 불러왔습니다.")
         return books
     except FileNotFoundError:
         print('File not found')
         return []
+
+def start_program():
+    print (
+        "\n=== 도서관 관리 시스템 ===\n"
+        "1. 도서 등록\n"
+        "2. 도서 목록\n"
+        "3. 회원 등록\n"
+        "4. 대출\n"
+        "5. 반납\n"
+        "6. 검색\n"
+        "7. 종료\n"
+    )
+    while True:
+        choice = input("메뉴를 선택하세요: ")
+        try:
+            choice = int(choice)
+        except ValueError:
+            print("1 ~ 7 사이 숫자를 입력하세요.")
+        else:
+            if 1 <= choice <= 7:
+                return choice
+            print("1 ~ 7 사이 숫자를 입력하세요.")
+
 
 
 def main():
@@ -23,6 +48,10 @@ def main():
     #     print(book)
 
     # 선택지 주기
+    start_program()
+    # print(f"선택지: {start_program()}")
+
+
     # 선택지 별 기능 구현
 
 
