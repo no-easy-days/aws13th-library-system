84d953e65470add2812b6f9f87225e7bdebd31f9
Eunice(Eunyoung) Shin <eunice.shin54@gmail.com>
2025-12-30 00:02:31 +0900

    feat: 1번 기능(도서 등록) 완성

---
+++ process_menu_choice.py
@@ -0,0 +1,38 @@
+from isoduration.parser.util import is_number
+
+from models import Book, Library
+
+def register_book(library):
+    print("\n[도서 등록]"
+          "\n도서 정보를 입력해주세요.")
+    title = input("책 제목: ")
+    author = input("저자: ")
+    while True:
+        isbn = input("isbn(숫자만 입력해주세요): ")
+        if not isbn.isdigit():
+            print("[ERROR] ISBN은 숫자만 입력해주세요.")
+            continue
+        if len(isbn) != 13:
+            print("[ERROR] ISBN은 13자리 입니다.")
+            continue
+        if library.check_same_isbn(isbn):
+            print("[ERROR] 이미 등록된 ISBN입니다.")
+            continue
+        library.add_book(Book(title, author, isbn))
+        print("\n도서를 등록하였습니다.")
+        break
+
+
+def process_menu_choice(library, choice):
+    if choice == 1:
+        register_book(library)
+    elif choice == 2:
+        return library
+    elif choice == 3:
+        return library
+    elif choice == 4:
+        return library
+    elif choice == 5:
+        return library
+    elif choice == 6:
+        return library
