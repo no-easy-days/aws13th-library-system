c698bde39eee848146cf61ee7aa3866bdc706dd9
Eunice(Eunyoung) Shin <eunice.shin54@gmail.com>
2025-12-29 19:13:24 +0900

    feat: add read_file func

---
+++ main.py
@@ -0,0 +1,27 @@
+from models import *
+import csv
+
+
+def read_file(filename):
+    books = []
+    with open(filename, newline='', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for title, author, isbn in reader:
+            books.append(Book(title, author, isbn))
+    return books
+
+
+def main():
+    # 파일 읽기
+    books = read_file('books.csv')
+    for book in books[:3]:
+        print(book)
+
+    # 선택지 주기
+    # 선택지 별 기능 구현
+
+
+
+if __name__ == "__main__":
+    main()
