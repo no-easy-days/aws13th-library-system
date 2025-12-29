98a11241ab4ef632075a3cbcc281156104e5315f
Eunice(Eunyoung) Shin <eunice.shin54@gmail.com>
2025-12-29 19:29:24 +0900

    feat: read_file func exception handling

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ import csv
 
 def read_file(filename):
     books = []
-    with open(filename, newline='', encoding='utf-8') as f:
-        reader = csv.reader(f)
-        next(reader)
-        for title, author, isbn in reader:
-            books.append(Book(title, author, isbn))
-    return books
+    try:
+        with open(filename, newline='', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for title, author, isbn in reader:
+                books.append(Book(title, author, isbn))
+        return books
+    except FileNotFoundError:
+        print('File not found')
+        return []
 
 
 def main():
     # 파일 읽기
     books = read_file('books.csv')
-    for book in books[:3]:
-        print(book)
+    # for book in books[:3]:
+    #     print(book)
 
     # 선택지 주기
     # 선택지 별 기능 구현
