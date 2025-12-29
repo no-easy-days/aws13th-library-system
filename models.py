b22532683f670b15e53f27139a491134e70fc27e
Eunice(Eunyoung) Shin <eunice.shin54@gmail.com>
2025-12-30 00:01:53 +0900

    feat: add Library method - check_same_isbn - add_book

--- models.py
+++ models.py
@@ -15,6 +15,15 @@ class Member:
         self.borrowed_books = borrowed_books
 
 class Library:
-    def __init__(self):
-        self.books = []
+    def __init__(self, books):
+        self.books = books
         self.members = {} # {"책 이름": "빌린 사람"}
+
+    def check_same_isbn(self, isbn):
+        for book in self.books:
+            if book.isbn == isbn:
+                return True
+        return False
+
+    def add_book(self, new_book):
+        self.books.append(new_book)
