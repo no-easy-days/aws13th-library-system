182805a5d3cc15398c6e2c70cf5911020b1dfc60
Kyunghk <kjsskkh01@kyonggi.ac.kr>
2025-12-30 15:55:11 +0900

    feat: implement book borrowing functionality

--- main.py
+++ main.py
@@ -62,10 +62,6 @@ for row in init_data:
     book = Book(row[0], row[1], row[2])
     library.add_book(book)
 
-# # 현재까지 추가된 책 목록 출력
-# for book in library.books:
-#     print(book)
-
 # 구현해야할 기능 : 도서 등록: 제목, 저자, ISBN 입력 받아 저장
 title = input("책 제목을 입력하세요: ")
 author = input("책 저자를 입력하세요: ")
@@ -84,3 +80,29 @@ name = input("회원 이름을 입력하세요: ")
 phone = input("회원 전화번호를 입력하세요: ")
 new_member = Member(name, phone, [])
 library.add_member(new_member)
+
+# 구현해야할 기능 : 도서 대출 : 회원 이름과 isbn 입력 받아 해당 회원 존재하고, 도서가 대출 가능하면 대출 처리
+member_name = input("사용자 이름을 입력하세요: ")
+isbn_to_borrow = input("대출할 책의 ISBN을 입력하세요: ")
+# 우선 회원 존재 여부 확인 및 도서 존재 여부 확인 및 대출 가능산 상태 확인
+# 회원 존재 여부
+if member_name in library.members:
+    member = library.members[member_name]
+    # 도서 존재 여부
+    book_to_borrow = None
+    for book in library.books:
+        if book.isbn == isbn_to_borrow:
+            book_to_borrow = book
+            break
+    if book_to_borrow:
+        # 도서 대출 상태 확인
+        if not book_to_borrow.is_borrowed:
+            library.borrow_book(member, book_to_borrow)
+            print(f">> '{member_name}'님이 '{book_to_borrow.title}' ({isbn_to_borrow})을 대출했습니다.")
+        else:
+            print("해당 도서는 이미 대출 중입니다.")
+    else:
+        print("해당 ISBN의 도서를 찾을 수 없습니다.")
+else:
+    print("해당 이름의 회원을 찾을 수 없습니다.")
+    
