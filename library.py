def2a608e6283a7fbcaac4aaa8738a249d02af10
Eunice(Eunyoung) Shin <eunice.shin54@gmail.com>
2026-01-04 01:41:33 +0900

    refactor: 도서 목록 출력 기능 수정

--- library.py
+++ library.py
@@ -17,15 +17,15 @@ class Library:
     #     else:
     #         print("[ERROR] 이미 등록된 회원입니다.")
     #         return
-    #
-    # def book_list(self):
-    #     if not self.books:
-    #         print("[ERROR] 등록된 도서가 없습니다.")
-    #         return
-    #     for book in self.books:
-    #         print(str(book))
-    #
-    #
+
+    def book_list(self) -> None:
+        if not self.books:
+            print("[ERROR] 등록된 도서가 없습니다.")
+            return
+        for book in self.books.values():
+            print(book)
+
+
     # # TODO: 여기부터, 기능 4 완성하면 3이랑 같이 커밋
     # def borrow_book(self, phone, isbn):
     #     for book in self.books:
