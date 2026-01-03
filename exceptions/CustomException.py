2d9068c0ce98cd68addc7f76b9e8278ae1d92f31
kim <youngwoo7804@gmail.com>
2026-01-03 20:32:54 +0900

    refact: File 입출력 시 예외 메서드 추가 및 예외처리 시 오류 수정

--- exceptions/CustomException.py
+++ exceptions/CustomException.py
@@ -5,14 +5,17 @@ class LibraryError (Exception):
 """
     파일 에러 (BookCsvRepository 에러)
 """
+
 class FileExtensionNotFound(LibraryError):
     def __init__(self, extension):
         self.extension = extension
         super().__init__(f"{extension}은 CSV 파일이 아닙니다.")
 
-class FileNotFoundError(LibraryError):
-    def __init__(self):
-        super().__init__(f"파일을 찾지 못했습니다.")
+#FileNotFoundError는 이미 예약된 클래스여서 오류발생
+class CustomFileNotFoundError(LibraryError):
+    def __init__(self,filePath):
+        self.filePath = filePath
+        super().__init__(f"{filePath}에 파일을 찾지 못했습니다.")
 
 """
     도서 관련 예외
@@ -33,11 +36,18 @@ class TargetBookIsBorrowed(BookError):
 class BookAlreadyExists(LibraryError):
     def __init__(self,book):
         self.book = book
+        super().__init__(f"{book.title}은 이미 책의 ISBN이 중복되어 등록이 불가능 합니다. ")
 
 class BookIsAlreadyReturned(BookError):
     def __init__(self,book):
         self.book = book
         super().__init__(f"{book}은 현재 대출 중이지 않습니다.")
+
+class NoMatchingBooksFound(BookError):
+    def __init__(self,title_keyword):
+        self.keyword = title_keyword
+        super().__init__(f"{self.keyword}에 일치하는 책을 찾을 수 없습니다. ")
+
 """
     멤버 관련 예외
 """
