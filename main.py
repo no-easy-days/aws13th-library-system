c690472e8d6f45bc722b789cf0b165365c602eeb
Eunice(Eunyoung) Shin <eunice.shin54@gmail.com>
2026-01-03 23:39:53 +0900

    refactor: 메뉴 출력 재설계된 클래스 맞춰 수정

--- main.py
+++ main.py
@@ -4,38 +4,38 @@ from library import Library
 from utils import load_books
 
 
-#
-# from process_menu_choice import process_menu_choice
-#
-#
-#
+from process_menu_choice import process_menu_choice
 
-#
-# def get_menu_choice():
-#     print (
-#         "\n=== 도서관 관리 시스템 ===\n"
-#         "1. 도서 등록\n"
-#         "2. 도서 목록\n"
-#         "3. 회원 등록\n"
-#         "4. 대출\n"
-#         "5. 반납\n"
-#         "6. 검색\n"
-#         "7. 종료\n"
-#     )
-#     while True:
-#         choice = input("메뉴를 선택하세요: ")
-#         try:
-#             choice = int(choice)
-#         # 문자 입력시 에러
-#         except ValueError:
-#             print("[ERROR] 1 ~ 7 사이 숫자를 입력하세요.")
-#         # 입력한 숫자의 범위가 맞지 않을 때 에러
-#         else:
-#             if 1 <= choice <= 7:
-#                 return choice
-#             print("[ERROR] 1 ~ 7 사이 숫자를 입력하세요.")
-#
-#
+def get_menu_choice() -> int:
+    """
+    사용자에게 기능 목록 제공
+
+    :return: 사용자가 선택한 기능 번호
+    """
+    print (
+        "\n=== 도서관 관리 시스템 ===\n"
+        "1. 도서 등록\n"
+        "2. 도서 목록\n"
+        "3. 회원 등록\n"
+        "4. 대출\n"
+        "5. 반납\n"
+        "6. 검색\n"
+        "7. 종료\n"
+    )
+    while True:
+        choice = input("메뉴를 선택하세요: ").strip()
+        try:
+            choice = int(choice)
+        # 문자 입력시 에러
+        except ValueError:
+            print("[ERROR] 1 ~ 7 사이 숫자를 입력하세요.")
+            continue
+        # 입력한 숫자의 범위가 맞지 않을 때 에러
+        if 1 <= choice <= 7:
+            return choice
+        print("[ERROR] 1 ~ 7 사이 숫자를 입력하세요.")
+
+
 def main():
     library = Library()
 
@@ -47,14 +47,13 @@ def main():
         return
     print(f"\n[System] books.csv 에서 도서 데이터를 불러왔습니다. (총 {count}권)")
 
-#
-#     # 선택지 주기
-#     while True:
-#         choice = get_menu_choice()
-#         if choice == 7:
-#             break
-#         process_menu_choice(library, choice)
-#
-#
+    # 기능 선택지 제공
+    while True:
+        choice = get_menu_choice()
+        if choice == 7:
+            break
+        process_menu_choice(library, choice)
+
+
 if __name__ == "__main__":
     main()
